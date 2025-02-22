9b2c76a165a1591243307911c67e4fcd3628049a
Kintoyyy_Nabu <56209108+Kintoyyy@users.noreply.github.com>
2025-02-23 01:17:52 +0800

    minor changes

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,6 +1,5 @@
-import { BiLogoNetlify } from "react-icons/bi";
 import { CgVercel } from "react-icons/cg";
-import { DiGithubAlt, DiMongodb } from "react-icons/di";
+import { DiMongodb } from "react-icons/di";
 import { FaDocker } from "react-icons/fa";
 import { BiSolidCoffee } from "react-icons/bi";
 import {
@@ -12,20 +11,8 @@ import {
   SiCloudways,
   SiBootstrap,
 } from "react-icons/si";
-import {
-  FaCss3Alt,
-  FaGitAlt,
-  FaHtml5,
-  FaNodeJs,
-  FaReact,
-  FaVuejs,
-  FaLaravel,
-  FaPhp,
-  FaJava,
-} from "react-icons/fa6";
-import { IoLogoJavascript } from "react-icons/io";
-import { SiExpress, SiPostman, SiTailwindcss, SiMysql, SiSqlite, SiArduino } from "react-icons/si";
-import { TbBrandCpp } from "react-icons/tb";
+import { FaGitAlt, FaNodeJs, FaReact, FaLaravel, FaJava } from "react-icons/fa6";
+import { SiPostman, SiTailwindcss, SiMysql, SiArduino } from "react-icons/si";
 
 function Skills() {
   return (
@@ -55,35 +42,23 @@ function Skills() {
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <SiCloudways className="text-2xl" /> Cloudways
         </span>
-        <span className="inline-flex items-center justify-between gap-x-2 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <FaHtml5 className="text-2xl" /> HTML
+        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
+          <CgVercel className="text-2xl" /> Vercel
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <FaCss3Alt className="text-2xl" /> CSS
+          <FaGitAlt className="text-2xl" /> Git
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <FaLaravel className="text-2xl" /> Laravel
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <FaPhp className="text-2xl" /> PHP
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <FaJava className="text-2xl" /> Java
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <TbBrandCpp className="text-2xl" /> C++
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <SiArduino className="text-2xl" /> Adruino
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <IoLogoJavascript className="text-2xl" /> JavaScript
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <FaReact className="text-2xl" /> React
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <SiExpress className="text-2xl" /> Express
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <FaNodeJs className="text-2xl" /> NodeJS
@@ -94,21 +69,7 @@ function Skills() {
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <SiBootstrap className="text-2xl" /> Bootstrap
         </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <FaVuejs className="text-2xl" /> VueJS
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <FaGitAlt className="text-2xl" /> Git
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <DiGithubAlt className="text-2xl" /> Github
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <BiLogoNetlify className="text-2xl" /> Netlify
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <CgVercel className="text-2xl" /> Vercel
-        </span>
+
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <SiPostman className="text-2xl" /> Postman
         </span>
@@ -117,9 +78,6 @@ function Skills() {
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <SiMysql className="text-2xl" /> MySQL
-        </span>
-        <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
-          <SiSqlite className="text-2xl" /> SQLite
         </span>
         <span className="inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-blue-100 text-blue-800 dark:bg-blue-800/30 dark:text-blue-500">
           <BiSolidCoffee className="text-2xl" /> Alot of Coffee
